2fe75611118cc572e79cd91312d64c21214251b9
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-04-04 15:04:38 +0500

    Рефакт шаблонов и передача им моковых данных

--- src/view/film-card.js
+++ src/view/film-card.js
@@ -1,16 +1,26 @@
-export const getFilmCardTemplate = () => {
+import dayjs from 'dayjs';
+import {LIMIT_CHAR_COMMENTS} from '../const';
+
+export const getFilmCardTemplate = (film, commentCount) => {
+
+  const {id, title, rating, release, time, genre, poster, description} = film;
+
+  const previewRelease = dayjs(release).format('YYYY');
+  const previewGenre = genre.join(' ');
+  const previewDescription = description.length > LIMIT_CHAR_COMMENTS ? description.slice(0, LIMIT_CHAR_COMMENTS) + '...' : description;
+
   return (
-    `<article class="film-card">
-      <h3 class="film-card__title">The Dance of Life</h3>
-      <p class="film-card__rating">8.3</p>
+    `<article class="film-card" data-id=${id}>
+      <h3 class="film-card__title">${title}</h3>
+      <p class="film-card__rating">${rating}</p>
       <p class="film-card__info">
-        <span class="film-card__year">1929</span>
-        <span class="film-card__duration">1h 55m</span>
-        <span class="film-card__genre">Musical</span>
+        <span class="film-card__year">${previewRelease}</span>
+        <span class="film-card__duration">${time}</span>
+        <span class="film-card__genre">${previewGenre}</span>
       </p>
-      <img src="./images/posters/the-dance-of-life.jpg" alt="" class="film-card__poster">
-      <p class="film-card__description">Burlesque comic Ralph "Skid" Johnson (Skelly), and specialty dancer Bonny Lee King (Carroll), end up together on a cold, rainy night at a tr…</p>
-      <a class="film-card__comments">5 comments</a>
+      <img src="./images/posters/${poster}" alt=${title} class="film-card__poster">
+      <p class="film-card__description">${previewDescription}</p>
+      <a class="film-card__comments">${commentCount} comments</a>
       <div class="film-card__controls">
         <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist" type="button">Add to watchlist</button>
         <button class="film-card__controls-item button film-card__controls-item--mark-as-watched" type="button">Mark as watched</button>
--- src/view/profile.js
+++ src/view/profile.js
@@ -1,7 +1,26 @@
-export const getProfileTemplate = () => {
+const getProfileRating = (watchedFilmsCount) => {
+
+  let profileRating = '';
+
+  if (watchedFilmsCount >= 1 && watchedFilmsCount <= 10) {
+    profileRating = 'Novice';
+  } else if (watchedFilmsCount >= 11 && watchedFilmsCount <= 20) {
+    profileRating = 'Fan';
+  } else if (watchedFilmsCount >= 21) {
+    profileRating = 'Movie Buff';
+  }
+
+  return profileRating;
+};
+
+export const getProfileTemplate = (films) => {
+
+  const watchedFilmsCount = films.filter((film) => film.isWatched === true).length;
+  const profileRating = getProfileRating(watchedFilmsCount);
+
   return (
     `<section class="header__profile profile">
-      <p class="profile__rating">Movie Buff</p>
+      <p class="profile__rating">${profileRating}</p>
       <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
     </section>`
   );
