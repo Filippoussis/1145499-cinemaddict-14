822c0ab57a48513e5a6a79d12ef4a845c4cf80b7
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-04-04 15:00:43 +0500

    Удалил отдельный файл utils, код разбил на разные файлы, см папку utils

--- src/utils.js
+++
@@ -1,5 +0,0 @@
-import {InsertPlace} from './const';
-
-export const renderViewComponent = (container, component, place = InsertPlace.BEFORE_END) => {
-  return container.insertAdjacentHTML(place, component);
-};
