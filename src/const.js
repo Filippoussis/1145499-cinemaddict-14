64b18bc5b05c940bb814aa3d1e37828519b9164d
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-04-04 15:01:30 +0500

    Рефакт const.js

--- src/const.js
+++ src/const.js
@@ -1,14 +1,11 @@
-const FilmCount = {
+export const FilmCount = {
   STEP: 5,
   EXTRA: 2,
 };
 
-const InsertPlace = {
+export const InsertPlace = {
   BEFORE_END: 'beforeend',
   AFTER_END: 'afterend',
 };
 
-export {
-  FilmCount,
-  InsertPlace
-};
+export const LIMIT_CHAR_COMMENTS = 140;
