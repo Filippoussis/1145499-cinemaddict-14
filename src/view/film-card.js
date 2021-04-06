48c35716b2f6ff52a83096059689cd68494e2773
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-04-06 14:00:22 +0500

    Рефакт film-card

--- src/view/film-card.js
+++ src/view/film-card.js
@@ -1,13 +1,17 @@
 import dayjs from 'dayjs';
-import {LIMIT_CHAR_COMMENTS} from '../const';
+import {DESCRIPTION_MAX_LENGTH, addClassModActive} from '../const';
 
-export const getFilmCardTemplate = (film, commentCount) => {
+export const getFilmCardTemplate = (film) => {
 
-  const {id, title, rating, release, time, genre, poster, description} = film;
+  const {id, title, rating, release, time, genres, poster, description, isWatchlist, isWatched, isFavorite, comments} = film;
 
   const previewRelease = dayjs(release).format('YYYY');
-  const previewGenre = genre.join(' ');
-  const previewDescription = description.length > LIMIT_CHAR_COMMENTS ? description.slice(0, LIMIT_CHAR_COMMENTS) + '...' : description;
+  const previewGenre = genres.join(' ');
+  const previewDescription = description.length > DESCRIPTION_MAX_LENGTH ? description.slice(0, DESCRIPTION_MAX_LENGTH - 1) + '\u2026' : description;
+  const isCheckedWatchlist = addClassModActive(isWatchlist);
+  const isCheckedWatched = addClassModActive(isWatched);
+  const isCheckedFavorite = addClassModActive(isFavorite);
+  const commentsCount = comments.length;
 
   return (
     `<article class="film-card" data-id=${id}>
@@ -18,13 +22,13 @@ export const getFilmCardTemplate = (film, commentCount) => {
         <span class="film-card__duration">${time}</span>
         <span class="film-card__genre">${previewGenre}</span>
       </p>
-      <img src="./images/posters/${poster}" alt=${title} class="film-card__poster">
+      <img src=${poster} alt=${title} class="film-card__poster">
       <p class="film-card__description">${previewDescription}</p>
-      <a class="film-card__comments">${commentCount} comments</a>
+      <a class="film-card__comments">${commentsCount} comments</a>
       <div class="film-card__controls">
-        <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist" type="button">Add to watchlist</button>
-        <button class="film-card__controls-item button film-card__controls-item--mark-as-watched" type="button">Mark as watched</button>
-        <button class="film-card__controls-item button film-card__controls-item--favorite" type="button">Mark as favorite</button>
+        <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist ${isCheckedWatchlist}" type="button">Add to watchlist</button>
+        <button class="film-card__controls-item button film-card__controls-item--mark-as-watched ${isCheckedWatched}" type="button">Mark as watched</button>
+        <button class="film-card__controls-item button film-card__controls-item--favorite ${isCheckedFavorite}" type="button">Mark as favorite</button>
       </div>
     </article>`
   );
