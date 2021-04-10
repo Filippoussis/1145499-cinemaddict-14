2a52c6540b1cec248ea9c24bf6646a50eb813445
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-04-10 21:39:40 +0500

    Перевел простые компоненты в классы

--- src/view/all-films.js
+++ src/view/all-films.js
@@ -1,8 +1,31 @@
-export const getAllFilmsTemplate = () => {
+import {createElement} from '../utils/render';
+
+const createAllFilmsTemplate = () => {
   return (
     `<section class="films-list" id="all-films">
       <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
-      <div class="films-list__container"></div>
     </section>`
   );
 };
+
+export default class AllFilms {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createAllFilmsTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
