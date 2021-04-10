3c1276e7acf1edeb1f53455eb0e40d1a865387ed
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-04-10 21:38:17 +0500

    Выделил в отдельный класс секцию films-container

---
+++ src/view/films-container.js
@@ -0,0 +1,30 @@
+import {createElement} from '../utils/render';
+
+const createFilmsContainerTemplate = () => {
+  return (
+    `<div class="films-list__container">
+    </div>`
+  );
+};
+
+export default class FilmsContainer {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createFilmsContainerTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
