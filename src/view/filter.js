c763b6b25b5ea3ed6edff945c1297318cc43d9ad
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-04-10 21:45:33 +0500

    Перевел фильтр в класс

--- src/view/filter.js
+++ src/view/filter.js
@@ -1,18 +1,38 @@
-export const getFilterTemplate = (films) => {
+import {createElement} from '../utils/render';
 
-  const watchlistCount = films.reduce((sum, {isWatchlist}) => sum + isWatchlist, 0);
-  const historyCount= films.reduce((sum, {isWatched}) => sum + isWatched, 0);
-  const favoriteCount = films.reduce((sum, {isFavorite}) => sum + isFavorite, 0);
-
+const createFilterTemplate = ({watchlistCount, watchedCount, favoriteCount}) => {
   return (
     `<nav class="main-navigation">
       <div class="main-navigation__items">
         <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
         <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">${watchlistCount}</span></a>
-        <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">${historyCount}</span></a>
+        <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">${watchedCount}</span></a>
         <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">${favoriteCount}</span></a>
       </div>
       <a href="#stats" class="main-navigation__additional">Stats</a>
     </nav>`
   );
 };
+
+export default class Filter {
+  constructor(filterResult) {
+    this._element = null;
+    this._filterResult = filterResult;
+  }
+
+  getTemplate() {
+    return createFilterTemplate(this._filterResult);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
