c85a1f4ecc7b04f503dcad99dcc12edcef01f27b
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-04-10 21:43:22 +0500

    Рефакт модуля render: удалил функцию renderViewComponent, создал render и createElement

--- src/utils/render.js
+++ src/utils/render.js
@@ -1,5 +1,19 @@
 import {InsertPlace} from '../const';
 
-export const renderViewComponent = (container, component, place = InsertPlace.BEFORE_END) => {
-  return container.insertAdjacentHTML(place, component);
+export const render = (container, element, place = InsertPlace.BEFORE_END) => {
+  switch (place) {
+    case InsertPlace.BEFORE_END:
+      container.append(element);
+      break;
+    case InsertPlace.AFTER_END:
+      container.after(element);
+      break;
+  }
+};
+
+export const createElement = (template) => {
+  const newElement = document.createElement('div');
+  newElement.innerHTML = template;
+
+  return newElement.firstChild;
 };
