58d100c987385f1e7148734764f2ad629c3ce856
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-04-13 20:16:20 +0500

    отнаследовал простые компоненты от абстрактного класса

--- src/view/main-content.js
+++ src/view/main-content.js
@@ -1,4 +1,4 @@
-import {createElement} from '../utils/render';
+import AbstractView from './abstract';
 
 const createMainContentTemplate = () => {
   return (
@@ -7,24 +7,8 @@ const createMainContentTemplate = () => {
   );
 };
 
-export default class MainContent {
-  constructor() {
-    this._element = null;
-  }
-
+export default class MainContent extends AbstractView {
   getTemplate() {
     return createMainContentTemplate();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
