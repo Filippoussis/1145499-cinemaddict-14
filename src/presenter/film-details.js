803771d2a3140a0c4cf7cd8b4b6f8d5a778bf4bd
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-04-23 21:23:13 +0500

    рефакт презентера film-details

--- src/presenter/film-details.js
+++ src/presenter/film-details.js
@@ -6,44 +6,37 @@ import FilmDetailsView from '../view/film-details';
 
 const NO_SCROLL_CLASS_NAME = 'hide-overflow';
 
-const Mode = {
-  DEFAULT: 'DEFAULT',
-  EDITING: 'EDITING',
-};
-
 export default class FilmDetailsPresenter {
   constructor(changeData) {
     this._changeData = changeData;
 
-    this._mode = Mode.DEFAULT;
-
     this._buttonEscKeyDownHandler = this._buttonEscKeyDownHandler.bind(this);
     this._removeFilmDetails = this._removeFilmDetails.bind(this);
-    this._toggleWatchlist = this._toggleWatchlist.bind(this);
-    this._toggleWatched = this._toggleWatched.bind(this);
-    this._toggleFavorite = this._toggleFavorite.bind(this);
+    this._handleWatchlist = this._handleWatchlist.bind(this);
+    this._handleWatched = this._handleWatched.bind(this);
+    this._handleFavorite = this._handleFavorite.bind(this);
   }
 
   init(film, comments) {
-    this._film = film;
+    this._film = Object.assign({}, film);
     this._filmDetailsView = new FilmDetailsView(this._film, comments);
 
     render(document.body, this._filmDetailsView);
-
-    this._mode = Mode.EDITING;
 
     this._addBodyNoScroll();
     this._addDocumentKeyDownHandler();
 
     this._filmDetailsView.setCloseButtonClickHandler(this._removeFilmDetails);
 
-    this._filmDetailsView.setWatchlistChangeHandler(this._toggleWatchlist);
-    this._filmDetailsView.setWatchedChangeHandler(this._toggleWatched);
-    this._filmDetailsView.setFavoriteChangeHandler(this._toggleFavorite);
+    this._filmDetailsView.setWatchlistChangeHandler(this._handleWatchlist);
+    this._filmDetailsView.setWatchedChangeHandler(this._handleWatched);
+    this._filmDetailsView.setFavoriteChangeHandler(this._handleFavorite);
+
+    this._isActive = true;
   }
 
   resetView() {
-    if (this._mode !== Mode.DEFAULT) {
+    if (this._isActive) {
       this._removeFilmDetails();
     }
   }
@@ -76,7 +69,7 @@ export default class FilmDetailsPresenter {
     this._removeDocumentKeyDownHandler();
   }
 
-  _toggleWatchlist() {
+  _handleWatchlist() {
     const newItem = Object.assign({}, this._film,
       {
         isWatchlist: !this._film.isWatchlist,
@@ -86,7 +79,7 @@ export default class FilmDetailsPresenter {
     this._film = newItem;
   }
 
-  _toggleWatched() {
+  _handleWatched() {
     const newItem = Object.assign({}, this._film,
       {
         isWatched: !this._film.isWatched,
@@ -96,7 +89,7 @@ export default class FilmDetailsPresenter {
     this._film = newItem;
   }
 
-  _toggleFavorite() {
+  _handleFavorite() {
     const newItem = Object.assign({}, this._film,
       {
         isFavorite: !this._film.isFavorite,
