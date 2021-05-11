b8406abbf3762f5d794fe3fea0c603a01e485e56
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-05-11 19:38:10 +0500

    рефакт view/films-sort

--- src/view/films-sort.js
+++ src/view/films-sort.js
@@ -2,25 +2,41 @@ import AbstractView from './abstract';
 
 import {SortType} from '../const';
 
-const createFilmsSortTemplate = () => {
+const createFilmsSortTemplate = (currentSortType) => {
   return (
     `<ul class="sort">
-      <li><a href="#" class="sort__button sort__button--active" data-sort-type="${SortType.DEFAULT}">Sort by default</a></li>
-      <li><a href="#" class="sort__button" data-sort-type="${SortType.DATE}">Sort by date</a></li>
-      <li><a href="#" class="sort__button" data-sort-type="${SortType.RATING}">Sort by rating</a></li>
+      <li>
+        <a href="#"
+          class="sort__button ${currentSortType === SortType.DEFAULT ? 'sort__button--active' : ''}"
+          data-sort-type="${SortType.DEFAULT}">Sort by default
+        </a>
+      </li>
+      <li>
+        <a href="#"
+          class="sort__button ${currentSortType === SortType.DATE ? 'sort__button--active' : ''}"
+          data-sort-type="${SortType.DATE}">Sort by date
+        </a>
+      </li>
+      <li>
+        <a href="#"
+          class="sort__button ${currentSortType === SortType.RATING ? 'sort__button--active' : ''}"
+          data-sort-type="${SortType.RATING}">Sort by rating
+        </a>
+      </li>
     </ul>`
   );
 };
 
 export default class FilmsSort extends AbstractView {
-  constructor() {
+  constructor(currentSortType) {
     super();
 
+    this._currentSortType = currentSortType;
     this._sortTypeChangeHandler = this._sortTypeChangeHandler.bind(this);
   }
 
   getTemplate() {
-    return createFilmsSortTemplate();
+    return createFilmsSortTemplate(this._currentSortType);
   }
 
   setSortTypeChangeHandler(callback) {
@@ -28,21 +44,12 @@ export default class FilmsSort extends AbstractView {
     this.getElement().addEventListener('click', this._sortTypeChangeHandler);
   }
 
-  _removeSortButtonActive() {
-    this.getElement().querySelector('.sort__button.sort__button--active').classList.remove('sort__button--active');
-  }
-
   _sortTypeChangeHandler(evt) {
+    evt.preventDefault();
+
     const target = evt.target;
     if (target.tagName !== 'A') {
       return;
-    }
-
-    evt.preventDefault();
-
-    if (!target.classList.contains('sort__button--active')) {
-      this._removeSortButtonActive();
-      target.classList.add('sort__button--active');
     }
 
     this._callback.changeSortType(target.dataset.sortType);
