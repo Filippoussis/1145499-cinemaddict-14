b716e6075204060b95801e266ddfc53769e950a1
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-05-11 19:38:48 +0500

    рефакт view/films-filter

--- src/view/films-filter.js
+++ src/view/films-filter.js
@@ -1,26 +1,58 @@
 import AbstractView from './abstract';
+import {FilterType} from '../const';
 
-const createFilmsFilterTemplate = ({watchlistCount, watchedCount, favoriteCount}) => {
+const createFilmsFilterTemplate = ({watchlistCount, watchedCount, favoriteCount}, currentFilterType) => {
   return (
-    `<nav class="main-navigation">
-      <div class="main-navigation__items">
-        <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
-        <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">${watchlistCount}</span></a>
-        <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">${watchedCount}</span></a>
-        <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">${favoriteCount}</span></a>
-      </div>
-      <a href="#stats" class="main-navigation__additional">Stats</a>
-    </nav>`
+    `<div class="main-navigation__items">
+      <a href="#all"
+        class="main-navigation__item ${currentFilterType === FilterType.ALL ? 'main-navigation__item--active' : ''}"
+        data-filter-type="${FilterType.ALL}">All movies</a>
+      <a href="#watchlist"
+        class="main-navigation__item ${currentFilterType === FilterType.WATCHLIST ? 'main-navigation__item--active' : ''}"
+        data-filter-type="${FilterType.WATCHLIST}">Watchlist
+        <span class="main-navigation__item-count">${watchlistCount}</span>
+      </a>
+      <a href="#history"
+        class="main-navigation__item ${currentFilterType === FilterType.WATCHED ? 'main-navigation__item--active' : ''}"
+        data-filter-type="${FilterType.WATCHED}">History
+        <span class="main-navigation__item-count">${watchedCount}</span>
+      </a>
+      <a href="#favorites"
+        class="main-navigation__item ${currentFilterType === FilterType.FAVORITES ? 'main-navigation__item--active' : ''}"
+        data-filter-type="${FilterType.FAVORITES}">Favorites
+        <span class="main-navigation__item-count">${favoriteCount}</span>
+      </a>
+    </div>`
   );
 };
 
 export default class FilmsFilter extends AbstractView {
-  constructor(value) {
+  constructor(filtersStats, currentFilterType) {
     super();
-    this._state = value;
+
+    this._filtersStats = filtersStats;
+    this._currentFilter = currentFilterType;
+
+    this._filterTypeChangeHandler = this._filterTypeChangeHandler.bind(this);
   }
 
   getTemplate() {
-    return createFilmsFilterTemplate(this._state);
+    return createFilmsFilterTemplate(this._filtersStats, this._currentFilter);
+  }
+
+  setFilterTypeChangeHandler(callback) {
+    this._callback.changeFilterType = callback;
+    this.getElement().addEventListener('click', this._filterTypeChangeHandler);
+  }
+
+  _filterTypeChangeHandler(evt) {
+    evt.preventDefault();
+
+    const target = evt.target;
+    if (target.tagName !== 'A') {
+      return;
+    }
+
+    this._callback.changeFilterType(target.dataset.filterType);
   }
 }
