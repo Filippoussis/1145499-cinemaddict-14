7483374912a04547a0f34ee8610936a8e22755ac
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-05-17 10:31:04 +0500

    предусмотрел hour === 0

--- src/utils/time.js
+++ src/utils/time.js
@@ -2,7 +2,9 @@ const MINUTES_PER_HOUR = 60;
 
 export const getFormattedTime = (time) => {
   const hours = Math.trunc(time/MINUTES_PER_HOUR);
-  const minutes = time - hours*MINUTES_PER_HOUR;
 
-  return `${hours}h ${minutes}m`;
+  const humanisedHours = hours > 0 ? `${hours}h` : '';
+  const humanisedMinutes = `${time - hours*MINUTES_PER_HOUR}m`;
+
+  return `${humanisedHours} ${humanisedMinutes}`;
 };
