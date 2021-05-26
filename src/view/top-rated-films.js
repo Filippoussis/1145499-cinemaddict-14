2b3aeb763e9875b2e6ec579296a18ee8cc3a63e3
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-05-26 13:24:45 +0500

    удалил неиспользуемые файлы

--- src/view/top-rated-films.js
+++
@@ -1,8 +0,0 @@
-export const getTopRatedFilmsTemplate = () => {
-  return (
-    `<section class="films-list films-list--extra" id="top-rated-films">
-      <h2 class="films-list__title">Top rated</h2>
-      <div class="films-list__container"></div>
-    </section>`
-  );
-};
