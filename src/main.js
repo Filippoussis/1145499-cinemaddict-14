1d797e9aaaaf438fb5be3080b6bf5de17ca8a33b
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2021-05-31 00:28:57 +0500

    рефакт main

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 // const
 import {UpdateType} from './const';
 
+// utils
+import {showDisconnectMessage} from './utils/toast';
+
 // model
 import FilmsModel from './model/films';
 import CommentsModel from './model/comments';
@@ -11,27 +14,50 @@ import SortModel from './model/sort';
 import ScreenPresenter from './presenter/screen';
 
 // api
-import Api from './api.js';
+import Api from './api/api';
+import Store from './api/store';
+import Provider from './api/provider';
 
 const AUTHORIZATION = 'Basic androsphilippos';
 const END_POINT = 'https://14.ecmascript.pages.academy/cinemaddict/';
 
+const STORE_PREFIX = 'cinemaddict-localstorage';
+const STORE_VER = 'v14';
+const STORE_NAME = `${STORE_PREFIX}-${STORE_VER}`;
+
 const body = document.querySelector('body');
 
 const api = new Api(END_POINT, AUTHORIZATION);
+
+const store = new Store(STORE_NAME, window.localStorage);
+const apiWithProvider = new Provider(api, store);
 
 const filmsModel = new FilmsModel();
 const commentsModel = new CommentsModel();
 const filterModel = new FilterModel();
 const sortModel = new SortModel();
 
-const screenPresenter = new ScreenPresenter(body, filmsModel, commentsModel, filterModel, sortModel, api);
+const screenPresenter = new ScreenPresenter(body, filmsModel, commentsModel, filterModel, sortModel, apiWithProvider);
 screenPresenter.init();
 
-api.getFilms()
+apiWithProvider.getFilms()
   .then((films) => {
     filmsModel.setItems(UpdateType.INIT, films);
   })
   .catch(() => {
     filmsModel.setItems(UpdateType.INIT, []);
   });
+
+window.addEventListener('load', () => {
+  navigator.serviceWorker.register('/sw.js');
+});
+
+window.addEventListener('online', () => {
+  document.title = document.title.replace(' [offline]', '');
+  apiWithProvider.sync();
+});
+
+window.addEventListener('offline', () => {
+  document.title += ' [offline]';
+  showDisconnectMessage('Check your internet connection...');
+});
